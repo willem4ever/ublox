4f4937497372e7662722fca30b680966c27b5106
Willem Eradus <willem@eradus.eu>
2016-03-24 21:31:40 +0100

    cleanup and other additions

--- ublox.cpp
+++ ublox.cpp
@@ -1,224 +1,259 @@
+/**
+ * Copyright 2016 Willem Eradus
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ **/
+
 #include "ublox.h"
 
+#include <Arduino.h>
+#include <stdio.h>
+#include <stdarg.h>
 
-void db_printf(const char *message,...) {
-	static char buffer[128];
-	va_list args;
-	va_start(args, message);
-	vsnprintf(buffer,128, message, args);
-	SerialUSB.print(buffer);
-	va_end(args);
+void uBlox::db_printf(const char *message,...) {
+    static char buffer[128];
+    va_list args;
+    va_start(args, message);
+    vsnprintf(buffer,128, message, args);
+    SerialUSB.print(buffer);
+    va_end(args);
 }
 
 uBlox::uBlox (TwoWire& Wire,uint8_t address):
-	_Wire(Wire) {
-	_address = address;
-	//
-	pinMode(GPS_ENABLE, OUTPUT);
-	pinMode(GPS_TIMEPULSE, INPUT);
+    _Wire(Wire) {
+    _address = address;
+    //
+    pinMode(GPS_ENABLE, OUTPUT);
+    pinMode(GPS_TIMEPULSE, INPUT);
 }
 
 void uBlox::enable () {
-	this->reset();
-	digitalWrite(GPS_ENABLE, 1);
-	db_printf("uBlox enabled\n");
+    this->reset();
+    digitalWrite(GPS_ENABLE, 1);
+    db_printf("uBlox enabled\n");
 }
 
 void uBlox::disable () {
-	this->reset();
-	digitalWrite(GPS_ENABLE, 0);
-	db_printf("uBlox disabled\n");
+    this->reset();
+    digitalWrite(GPS_ENABLE, 0);
+    db_printf("uBlox disabled\n");
 }
 
 void uBlox::flush() {
-	this->reset();
-	uint16_t bytes = this->available();
-	if (bytes) {
-		Wire.requestFrom(_address, bytes);
-		do {
-			(void) Wire.read();
-		} while (--bytes);
-	}
+    this->reset();
+    uint16_t bytes = this->available();
+    if (bytes) {
+        Wire.requestFrom(_address, bytes);
+        do {
+            (void) Wire.read();
+        } while (--bytes);
+    }
 }
 
 void uBlox::reset() {
-	state = 0;
+    state = 0;
 }
 
 int uBlox::process(uint8_t c) {
-	static uint8_t ck_a,ck_b;
-	if (state == 0 && c == 0xb5)
-		state = 1;
-	else if (state == 1) {
-		if (c == 0x62)
-			state = 2;
-		else
-			state = 0;
-	}
-	else if (state == 2) {
-		ck_a = c; ck_b = c;
-		state = 3;
-		Id = (uint16_t) c << 8;
-	}
-	else if (state == 3) {
-		ck_a += c; ck_b += ck_a;
-		state = 4;
-		Id |= (uint16_t) c;
-		// db_printf("Id=%4.4x\n",Id);
-	}
-	else if (state == 4) {
-		ck_a += c; ck_b += ck_a;
-		plLength = c;
-		state = 5;
-	}
-	else if (state == 5) {
-		ck_a += c; ck_b += ck_a;
-		plLength |= (uint16_t) c << 8;
-		state = 6;
-		if (Id == 0x500 || Id == 0x501)	// ACK/NAK buffer
-			p = (uint8_t *) &AckedId;
-		else {
-			payLoad.length = plLength;
-			p = payLoad.buffer;
-		}
-	}
-	else if (state == 6) {
-		ck_a += c; ck_b += ck_a;
-		*p++ = c;
-		if (--plLength == 0) {
-			state = 7;
-		}
-	}
-	else if (state == 7) {
-		if (c == ck_a)
-			state=8;
-		else
-			state = 0;
-	}
-	else if (state == 8) {
-		state = 0;
-		if (ck_b == c) {
-			if (Id == 0x0107) {
-				NavPvt = (NavigationPositionVelocityTimeSolution*) payLoad.buffer;
-				db_printf("%4.4d-%2.2d-%2.2d %2.2d:%2.2d:%2.2d.%d lat=%d lon=%d sats=%d\n",
-						  NavPvt->year,NavPvt->month,NavPvt->day,
-						  NavPvt->hour,NavPvt->minute,NavPvt->seconds,NavPvt->nano,
-						  NavPvt->lat,NavPvt->lon,NavPvt->numSV);
-			}
-			else if (Id == 0x0501 || Id == 0x500) {
-				AckedId = (AckedId >> 8) | (AckedId << 8);	// Change Endianess
-				db_printf("Id=%4.4x Ack=%4.4x\n",Id,AckedId);
-			}
-			else if (Id == 0x0631) {
-				CfgTp = (TimePulseParameters*) payLoad.buffer;
-				db_printf("freqPeriod=%d freqPeriodLock=%d pulseLenRatio=%d pulseLenRatioLock=%d flags=%4.4x\n",
-						  CfgTp->freqPeriod,
-						  CfgTp->freqPeriodLock,
-						  CfgTp->pulseLenRatio,
-						  CfgTp->pulseLenRatioLock,
-						  CfgTp->flags);
-			}
-			else
-				db_printf("Processed Id=0x%4.4x\n",Id);
-			return Id;
-
-		}
-	}
-	return (0-state);
+    static uint8_t ck_a,ck_b;
+    if (state == 0 && c == 0xb5)
+        state = 1;
+    else if (state == 1) {
+        if (c == 0x62)
+            state = 2;
+        else
+            state = 0;
+    }
+    else if (state == 2) {
+        ck_a = c; ck_b = c;
+        state = 3;
+        Id = (uint16_t) c << 8;
+    }
+    else if (state == 3) {
+        ck_a += c; ck_b += ck_a;
+        state = 4;
+        Id |= (uint16_t) c;
+        // db_printf("Id=%4.4x\n",Id);
+    }
+    else if (state == 4) {
+        ck_a += c; ck_b += ck_a;
+        plLength = c;
+        state = 5;
+    }
+    else if (state == 5) {
+        ck_a += c; ck_b += ck_a;
+        plLength |= (uint16_t) c << 8;
+        state = 6;
+        if (Id == 0x500 || Id == 0x501) // ACK/NAK buffer
+            p = (uint8_t *) &AckedId;
+        else {
+            payLoad.length = plLength;
+            p = payLoad.buffer;
+        }
+    }
+    else if (state == 6) {
+        ck_a += c; ck_b += ck_a;
+        *p++ = c;
+        if (--plLength == 0) {
+            state = 7;
+        }
+    }
+    else if (state == 7) {
+        if (c == ck_a)
+            state=8;
+        else
+            state = 0;
+    }
+    else if (state == 8) {
+        state = 0;
+        if (ck_b == c) {
+            if (Id == 0x0501 || Id == 0x500) {
+                AckedId = (AckedId >> 8) | (AckedId << 8);  // Change Endianess
+                // db_printf("Id=%4.4x Ack=%4.4x\n",Id,AckedId);
+            }
+            return Id;
+        }
+    }
+    return (0-state);
 }
 
 int uBlox::available() {
-	_Wire.beginTransmission(_address);
-	_Wire.write((uint8_t)0xfd);
-	_Wire.endTransmission(false);
-	_Wire.requestFrom(_address, 2);//
-	uint16_t bytes = (uint16_t) _Wire.read() << 8;
-	bytes |= _Wire.read();
-	return bytes;
+    _Wire.beginTransmission(_address);
+    _Wire.write((uint8_t)0xfd);
+    _Wire.endTransmission(false);
+    _Wire.requestFrom(_address, 2);//
+    uint16_t bytes = (uint16_t) _Wire.read() << 8;
+    bytes |= _Wire.read();
+    return bytes;
 }
 
 int uBlox::send(uint8_t *buffer,int n) {
-	uint8_t ck_a,ck_b;
-	ck_a = ck_b = 0;
-	//
-	for (int i=0;i<n;i++) {
-		ck_a += buffer[i];
-		ck_b += ck_a;
-	}
-	//
-	_Wire.beginTransmission(_address);
-	_Wire.write(0xb5);
-	_Wire.write(0x62);
-	_Wire.write(buffer, n);
-	_Wire.write(ck_a);
-	_Wire.write(ck_b);
-	return _Wire.endTransmission();
+    uint8_t ck_a,ck_b;
+    ck_a = ck_b = 0;
+    //
+    for (int i=0;i<n;i++) {
+        ck_a += buffer[i];
+        ck_b += ck_a;
+    }
+    //
+    _Wire.beginTransmission(_address);
+    _Wire.write(0xb5);
+    _Wire.write(0x62);
+    _Wire.write(buffer, n);
+    _Wire.write(ck_a);
+    _Wire.write(ck_b);
+    return _Wire.endTransmission();
 }
 
-void uBlox::CfgMsg(uint8_t MsgClass, uint8_t MsgID,uint8_t rate) {
-	uint8_t buffer[7];
-	//
-	buffer[0] = 0x06;
-	buffer[1] = 0x01;
-	buffer[2] = 0x03;
-	buffer[3] = 0;
-	buffer[4] = MsgClass;
-	buffer[5] = MsgID;
-	buffer[6] = rate;
-	// Push message on Wire
-	int i = this->send(buffer,7);
-	this->wait();
+void uBlox::CfgMsg(uint16_t Msg,uint8_t rate) {
+    uint8_t buffer[7];
+    //
+    buffer[0] = 0x06;
+    buffer[1] = 0x01;
+    buffer[2] = 0x03;
+    buffer[3] = 0;
+    buffer[4] = (Msg >> 8) & 0xff;
+    buffer[5] = Msg & 0xff;
+    buffer[6] = rate;
+    // Push message on Wire
+    int i = this->send(buffer,7);
+    this->wait();
 }
+
+int uBlox::CfgPrt () {
+    uint8_t buffer[4];
+    //
+    buffer[0] = 0x06;
+    buffer[1] = 0x00;
+    buffer[2] = 0;
+    buffer[3] = 0;
+    // Push message on Wire
+    int i = this->send(buffer,4);
+    return this->wait();
+}
+
+int uBlox::CfgPrt (PortConfigurationDDC *pcd) {
+    // Warning this overwrites the receive buffer !!!
+    payLoad.buffer[0] = 0x06;
+    payLoad.buffer[1] = 0x00;
+    payLoad.buffer[2] = 20;
+    payLoad.buffer[3] = 0;
+    memcpy(&payLoad.buffer[4],(uint8_t*)pcd,20);
+    // Push message on Wire
+    int i = this->send(payLoad.buffer,24);
+    return this->wait();
+}
+
 
 int uBlox::CfgTp5 (uint8_t tpIdx) {
-	uint8_t buffer[5];
-	//
-	buffer[0] = 0x06;
-	buffer[1] = 0x31;
-	buffer[2] = 1;
-	buffer[3] = 0;
-	buffer[4] = tpIdx;
-	// Push message on Wire
-	int i = this->send(buffer,5);
-	return this->wait();
+    uint8_t buffer[5];
+    //
+    buffer[0] = 0x06;
+    buffer[1] = 0x31;
+    buffer[2] = 1;
+    buffer[3] = 0;
+    buffer[4] = tpIdx;
+    // Push message on Wire
+    int i = this->send(buffer,5);
+    return this->wait();
 }
 
 int uBlox::CfgTp5 (TimePulseParameters *Tpp) {
-	// Warning this overwrites the receive buffer !!!
-	payLoad.buffer[0] = 0x06;
-	payLoad.buffer[1] = 0x31;
-	payLoad.buffer[2] = 32;
-	payLoad.buffer[3] = 0;
-	memcpy(&payLoad.buffer[4],(uint8_t*)Tpp,32);
-	// Push message on Wire
-	int i = this->send(payLoad.buffer,36);
-	return this->wait();
+    // Warning this overwrites the receive buffer !!!
+    payLoad.buffer[0] = 0x06;
+    payLoad.buffer[1] = 0x31;
+    payLoad.buffer[2] = 32;
+    payLoad.buffer[3] = 0;
+    memcpy(&payLoad.buffer[4],(uint8_t*)Tpp,32);
+    // Push message on Wire
+    int i = this->send(payLoad.buffer,36);
+    return this->wait();
 }
 
-uint8_t *uBlox::getBuffer (uint16_t required) {
-	if (payLoad.length == required) // Should at least match
-		return payLoad.buffer;
-	else
-		return NULL;
+void *uBlox::getBuffer (uint16_t required) {
+    if (payLoad.length == required) // Should at least match
+        return payLoad.buffer;
+    else {
+        this->db_printf("Required=%d, available=%d\n",required,payLoad.length);
+        return NULL;
+    }
+}
+
+uint8_t uBlox::GetResponse(void *d,uint16_t required) {
+    if (payLoad.length == required) {
+        memcpy(d,payLoad.buffer,required);
+        return required;
+    }
+    return 0;
 }
 
 uint16_t uBlox::getAckedId () {
-	return AckedId;
+    return AckedId;
 }
 
 int uBlox::wait() {
-	uint32_t s = millis(),elapsed;
-	uint16_t bytes;
-	int16_t	id = 0;
-	// Wait for (N)ACK
-	while ((elapsed = millis()-s) < 50 && (bytes = this->available()) == 0 );
-	if (bytes) {
-		// db_printf("Waited %d ms for %d bytes\n",elapsed,bytes);
-		if (Wire.requestFrom(_address, bytes)) {
-			do {
-				id = this->process(_Wire.read());
-			} while (--bytes);
-		}
-	}
-	return id;
+    uint32_t s = millis(),elapsed;
+    uint16_t bytes;
+    int16_t id = 0;
+    // Wait 50 ms for response
+    while ((elapsed = millis()-s) < 50 && (bytes = this->available()) == 0 );
+    if (bytes) {
+        // db_printf("Waited %d ms for %d bytes\n",elapsed,bytes);
+        if (Wire.requestFrom(_address, bytes)) {
+            do {
+                id = this->process(_Wire.read());
+            } while (--bytes);
+        }
+    }
+    return id;
 }
--- ublox.h
+++ ublox.h
@@ -1,121 +1,158 @@
+/**
+ * Copyright 2016 Willem Eradus
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ **/
 
 #ifndef UBLOX_H
 #define UBLOX_H
 
-#include "Arduino.h"
-#include "Wire.h"
-#include <stdio.h>
-#include <stdarg.h>
-#include <math.h>
+#include <Wire.h>
 
 typedef struct __attribute__((packed,aligned(1))) NavigationPositionVelocityTimeSolution {
-	uint32_t	iTOW;		// GPS time of week of the navigation epoch.
-	uint16_t	year;		// Year UTC
-	uint8_t		month;		// Month, range 1..12 (UTC)
-	uint8_t		day;		// Day of month, range 1..31 (UTC)
-	uint8_t		hour;		// Hour of day, range 0..23 (UTC)
-	uint8_t		minute;		// Minute of hour, range 0..59 (UTC)
-	uint8_t		seconds;	// Seconds of minute, range 0..60 (UTC)
-	uint8_t		valid;		// Validity Flags (see graphic below)
-	uint32_t	tAcc;		// Time accuracy estimate (UTC)
-	int32_t		nano;		// Fraction of second, range -1e9 .. 1e9 (UTC)
-	uint8_t		fixType;	// GNSSfix Type, range 0..5
-	uint8_t		flags;		// Fix Status Flags
-	uint8_t		reserved1;	// Reserved
-	uint8_t		numSV;		// Number of satellites used in Nav Solution
-	int32_t		lon;		// Longitude
-	int32_t		lat;		// Latitude
-	int32_t		height;		// Height above Ellipsoid
-	int32_t		hMSL;		// Height above mean sea level
-	uint32_t	hAcc;		// Horizontal Accuracy Estimate
-	uint32_t	vAcc;		// Vertical Accuracy Estimate
-	int32_t		velN;		// NED north velocity
-	int32_t		velE;		// NED east velocity
-	int32_t		velD;		// NED down velocity
-	int32_t		gSpeed;		// Ground Speed (2-D)
-	int32_t		heading;	// Heading of motion 2-D
-	uint32_t	sAcc;		// Speed Accuracy Estimate
-	int32_t		headingAcc;	// Heading Accuracy Estimate
-	int32_t		pDOP;		// Position DOP
-	uint16_t	reserved2;	// Position DOP
-	uint32_t	reserved3;	// Reserved
+    uint32_t    iTOW;       // 00 GPS time of week of the navigation epoch.
+    uint16_t    year;       // 04 Year UTC
+    uint8_t     month;      // 06 Month, range 1..12 (UTC)
+    uint8_t     day;        // 07 Day of month, range 1..31 (UTC)
+    uint8_t     hour;       // 08 Hour of day, range 0..23 (UTC)
+    uint8_t     minute;     // 09 Minute of hour, range 0..59 (UTC)
+    uint8_t     seconds;    // 10 Seconds of minute, range 0..60 (UTC)
+    uint8_t     valid;      // 11 Validity Flags (see graphic below)
+    uint32_t    tAcc;       // 12 Time accuracy estimate (UTC)
+    int32_t     nano;       // 16 Fraction of second, range -1e9 .. 1e9 (UTC)
+    uint8_t     fixType;    // 20 GNSSfix Type, range 0..5
+    uint8_t     flags;      // 21 Fix Status Flags
+    uint8_t     reserved1;  // 22 Reserved
+    uint8_t     numSV;      // 23 Number of satellites used in Nav Solution
+    int32_t     lon;        // 24 Longitude
+    int32_t     lat;        // 28 Latitude
+    int32_t     height;     // 32 Height above Ellipsoid
+    int32_t     hMSL;       // 36 Height above mean sea level
+    uint32_t    hAcc;       // 40 Horizontal Accuracy Estimate
+    uint32_t    vAcc;       // 44 Vertical Accuracy Estimate
+    int32_t     velN;       // 48 NED north velocity
+    int32_t     velE;       // 52 NED east velocity
+    int32_t     velD;       // 56 NED down velocity
+    int32_t     gSpeed;     // 60 Ground Speed (2-D)
+    int32_t     heading;    // 64 Heading of motion 2-D
+    uint32_t    sAcc;       // 68 Speed Accuracy Estimate
+    uint32_t    headingAcc; // 72 Heading Accuracy Estimate
+    uint16_t    pDOP;       // 76 Position DOP
+    uint16_t    reserved2;  // 78 Reserved
+    uint32_t    reserved3;  // 80 Reserved
 } NavigationPositionVelocityTimeSolution;
 
+
+typedef struct __attribute__((packed,aligned(1))) PortConfigurationDDC {
+    uint8_t     portID;
+    uint8_t     reserved0;
+    uint16_t    txReady;
+    uint32_t    mode;
+    uint32_t    reserved3;
+    uint16_t    inProtoMask;
+    uint16_t    outProtoMask;
+    uint16_t    flags;
+    uint16_t    reserved5;
+} PortConfigurationDDC;
+
 typedef struct __attribute__((packed,aligned(1))) TimePulseParameters {
-	uint8_t		tpIdx;
-	uint8_t		reserved0;
-	uint16_t	reserved1;
-	int16_t		antCableDelay;
-	int16_t		rfGroupDelay;
-	uint32_t	freqPeriod;
-	uint32_t	freqPeriodLock;
-	uint32_t	pulseLenRatio;
-	uint32_t	pulseLenRatioLock;
-	int32_t		userConfigDelay;
-	uint32_t	flags;
+    uint8_t     tpIdx;
+    uint8_t     reserved0;
+    uint16_t    reserved1;
+    int16_t     antCableDelay;
+    int16_t     rfGroupDelay;
+    uint32_t    freqPeriod;
+    uint32_t    freqPeriodLock;
+    uint32_t    pulseLenRatio;
+    uint32_t    pulseLenRatioLock;
+    int32_t     userConfigDelay;
+    uint32_t    flags;
 
 } TimePulseParameters;
 
+typedef struct __attribute__((packed,aligned(1))) TimePulseTimedata {
+    uint32_t    towMS;          // ms Time pulse time of week according to time base
+    uint32_t    towSubMS;       // ms Submillisecond part of TOWMS
+    int32_t     qErr;           // ps Quantization error of time pulse.
+    uint16_t    week;           // weeks Time pulse week number according to time base
+    uint8_t     flags;
+    uint8_t     reserved1;
+} TimePulseTimedata;
+
 enum NMEA {
-	NMEA_CGA = 0,
-	NMEA_GLL,
-	NMEA_GSA,
-	NMEA_GSV,
-	NMEA_RMC,
-	NMEA_VTG,
-	NMEA_GRS,
-	NMEA_GST,
-	NMEA_ZDA,
-	NMEA_GBS,
-	NMEA_DTM,
-	NMEA_GNS = 0xd,
-	NMEA_GPQ = 0x40,
-	NMEA_TXT,
-	NMEA_GNQ,
-	NMEA_GLQ,
-	NMEA_STD = 0xf0,
-	NMEA_PBX
+    UBX_NAV_PVT = 0x0107,
+    UBX_TIM_TP  = 0x0d01,
+    NMEA_CGA    = 0xf000,
+    NMEA_GLL,
+    NMEA_GSA,
+    NMEA_GSV,
+    NMEA_RMC,
+    NMEA_VTG,
+    NMEA_GRS,
+    NMEA_GST,
+    NMEA_ZDA,
+    NMEA_GBS,
+    NMEA_DTM,
+    NMEA_GNS    = 0xf00d,
+    NMEA_GPQ    = 0xf040,
+    NMEA_TXT,
+    NMEA_GNQ,
+    NMEA_GLQ
 };
 
 class uBlox {
 
 public:
-	NavigationPositionVelocityTimeSolution *NavPvt;
-	TimePulseParameters *CfgTp;
-	//
-	uBlox	(TwoWire& ,uint8_t);
-	void	CfgMsg(uint8_t,uint8_t, uint8_t);
-	int		CfgTp5 (uint8_t);
-	int		CfgTp5 (TimePulseParameters*);
+    NavigationPositionVelocityTimeSolution *NavPvt;
+    TimePulseParameters *CfgTp;
+    //
+    uBlox   (TwoWire& Wire,uint8_t address);
+    void    CfgMsg(uint16_t Msg,uint8_t rate);
+    int     CfgPrt ();
+    int     CfgPrt (PortConfigurationDDC *pcd);
+    int     CfgTp5 (uint8_t tpIdx);
+    int     CfgTp5 (TimePulseParameters *Tpp);
 
-	uint8_t *getBuffer(uint16_t);
-	uint16_t getAckedId ();
-	int		available();
-	int		process(uint8_t);
-	void	enable ();
-	void	disable ();
-	void	flush();
-	void	reset();
+    void    *getBuffer (uint16_t required);
+    uint8_t GetResponse(void *d,uint16_t required);
+    uint16_t getAckedId ();
+    int     available();
+    int     process(uint8_t);
+    void    enable ();
+    void    disable ();
+    void    flush();
+    void    reset();
+    // Debug helper
+    void    db_printf(const char *message,...);
 
 private:
-	int send(uint8_t *,int);
-	int wait();
-	//
-	TwoWire&	_Wire;
-	uint8_t		_address;
-	int			state = 0;
-	uint16_t	AckedId;
-	uint16_t	plLength;
-	uint16_t	Id;
-	uint8_t		*p;
-	struct {
-		uint16_t length;
-		uint8_t	buffer[256];
-	} payLoad;
+    int         send(uint8_t *buffer,int n);
+    int         wait();
+    //
+    TwoWire&    _Wire;
+    uint8_t     _address;
+    uint8_t     *p;
+    int         state = 0;
+    uint16_t    AckedId;
+    uint16_t    plLength;
+    uint16_t    Id;
 
-
+    struct {
+        uint16_t length;
+        uint8_t buffer[256];
+    } payLoad;
+    
+    
 };
-
-
 
 #endif // UBLOX_H
